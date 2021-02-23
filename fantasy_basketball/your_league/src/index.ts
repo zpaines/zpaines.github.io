c9f1378aedfef68e221f5a5fb8f16549c7b273be
Zach Sabin <zacharysabin@gmail.com>
2021-02-23 08:10:05 -0800

    fix bug, update stats

--- fantasy_basketball/your_league/src/index.ts
+++ fantasy_basketball/your_league/src/index.ts
@@ -14,7 +14,7 @@ console.log(Object.keys(playerTeams).length);
 if (Object.keys(playerTeams).length < 1) {
   console.log(Object.keys(playerTeams))
   playerTeams = require('../players.json');
-  // document.getElementById('instructions').style.removeProperty('display')
+  document.getElementById('instructions').style.removeProperty('display')
 }
 let comparison_chart = new ComparisonChart(playerData, document.getElementById('stat-comparison'), playerTeams, 'FG%');
 let head_to_head = new HeadToHeadChart(playerData, document.getElementById('head-to-head'), playerTeams);
